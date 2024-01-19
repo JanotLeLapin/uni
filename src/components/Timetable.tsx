b6545267128d8e84c39f3e71a5e13dac74a80b73
Joseph DALY <joseph300905@gmail.com>
2024-01-19 02:47:31 +0100

    timetable prototype

--- src/components/Timetable.tsx
+++ src/components/Timetable.tsx
@@ -1,9 +1,25 @@
-import { fetchTimetable, parseDescription, type TTEntry } from "@lib/timetable";
-import { useEffect, useState } from "preact/hooks"
+import { fetchTimetable, type TTEntry } from "@lib/timetable";
+import { useEffect, useState } from "preact/hooks";
+
+import "@style/timetable.css";
 
 export default () => {
   const [timetable, setTimetable] = useState<TTEntry[]>([]);
-  fetchTimetable("S2MITD-01", new Date()).then(setTimetable);
 
-  return (<></>)
+  useEffect(() => {
+    fetchTimetable("S2MITD-01", new Date(2024, 0, 23)).then(setTimetable);
+  }, []);
+
+  return (
+    <div class="timetable">
+      {timetable.map(tt => (
+        <div
+          key={tt.id}
+          class="event"
+          style={`--day: ${tt.start.getDay()}; --schedule: ${tt.start.getHours() - 7}`}>
+            <p>{tt.description.subject}</p>
+          </div>
+      ))}
+    </div>
+  )
 }
--- src/lib/timetable.ts
+++ src/lib/timetable.ts
@@ -1,36 +1,45 @@
-export type TTEntry = {
-  id: string;
-  start: Date;
-  end: Date;
-  allDay: boolean;
-  description: string;
-  backgroundColor: string;
-  textColor: string;
-  eventCategory: string;
-  sites: string[];
-}
-
 export type TTDescription = {
   type: string;
   location: string;
   subject: string;
 };
 
+export type TTEntry = {
+  id: string;
+  start: Date;
+  end: Date;
+  allDay: boolean;
+  description: TTDescription;
+  backgroundColor: string;
+  textColor: string;
+  eventCategory: string;
+  sites: string[];
+}
+
 const decodeHTML = (raw: string) => raw.replace(/&#(\d+);/g, (_, dec) => String.fromCharCode(dec));
+export const parseDescription = (description: string) => {
+  const [type, location, subject] = description.split("\r\n\r\n<br />\r\n\r\n").map(decodeHTML);
+  return { type, location, subject };
+}
 
 export const fetchTimetable = async (id: string, date: Date): Promise<TTEntry[]> => {
+  const d = new Date(date.getFullYear(), date.getMonth(), date.getDate() - date.getDay());
+
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const url = `https://corsproxy.io/?${encodeURIComponent("https://edt.uvsq.fr/Home/GetCalendarData")}`;
-  const body = `start=${year}-${month}-1&end=${year}-${month + 1}-1&resType=103&calview=month&federationIds%5B%5D=${id}&colourScheme=3`;
-  return await fetch(url, {
+  const body = `start=${year}-${month}-${d.getDate()}&end=${year}-${month}-${d.getDate() + 5}&resType=103&calview=month&federationIds%5B%5D=${id}&colourScheme=3`;
+  const json = await fetch(url, {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8", },
     body
   }).then(res => res.json())
-}
 
-export const parseDescription = (description: string) => {
-  const [type, location, subject] = description.split("\r\n\r\n<br />\r\n\r\n").map(decodeHTML);
-  return { type, location, subject };
+  return json
+    .map((tt: any) => ({
+      ...tt,
+      description: parseDescription(tt.description),
+      start: new Date(tt.start),
+      end: new Date(tt.end),
+    })) as TTEntry[];
 }
