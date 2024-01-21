47eecd12c4e7b8ca3606ca1c833d8d6cfa424a0f
Joseph DALY <joseph300905@gmail.com>
2024-01-22 00:38:05 +0100

    github ci

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import preact from '@astrojs/preact';
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact()],
+  site: 'https://janotlelapin.github.io',
   base: '/uni',
   markdown: {
     remarkPlugins: ['remark-math'],
