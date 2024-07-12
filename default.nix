4b9f01b1b8d638e271832b84d43752d80d0ec94c
Joseph DALY <joseph300905@gmail.com>
2024-07-12 16:07:05 +0200

    basic html, css base

--- default.nix
+++ default.nix
@@ -5,11 +5,18 @@
   src = ./content;
   buildInputs = [ markdown ];
   buildPhase = ''
+    base=$(tr -d '\n' < ${./base.html})
     find "$src" -type f -name "*.md" | while read -r file; do
       rel_path="''${file#$src/}"
       dest_file="$out/''${rel_path%.md}.html"
+      content=$(markdown "$file")
       mkdir -p "$(dirname "$dest_file")"
-      markdown "$file" > "$dest_file"
+      echo "''${base/\{\}/$content}" > "$dest_file"
     done
+  '';
+
+  installPhase = ''
+    mkdir -p $out/static
+    cp ${./base.css} $out/static/base.css
   '';
 }
