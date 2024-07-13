dec4dc5d0cc6fdf3edc338a66d9ee183556e54a9
Joseph DALY <joseph300905@gmail.com>
2024-07-13 16:15:43 +0200

    partial syntax highlighting

--- config.h
+++ config.h
@@ -1,2 +1,7 @@
 #define TOC 1
 #define TOC_TITLE "Contenu"
+
+static const char *c_kw[] = { "auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else", "enum", "extern", "float", "for", "goto", "if", "int", "long", "register", "return", "short", "signed", "sizeof", "static", "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while", NULL };
+static const Lang langs[] = {
+  "c", c_kw,
+};
