655402cbfd82a02bad3dbfe248512a5a08825614
Joseph DALY <joseph300905@gmail.com>
2024-09-01 14:02:17 +0200

    add output stream

--- compiler/uni.c
+++ compiler/uni.c
@@ -3,32 +3,32 @@
 #include <tree_sitter/api.h>
 #include <stdio.h>
 
-#define COMPILE_CHILDREN(i, node) for (i = 0; i < node.children_count; i++) compile_node(node.children[i]);
+#define COMPILE_CHILDREN(i, node, stream) for (i = 0; i < node.children_count; i++) compile_node(node.children[i], stream);
 
 const TSLanguage *tree_sitter_python(void);
 const TSLanguage *tree_sitter_c(void);
 
 void
-putchar_escaped(char c)
+putchar_escaped(char c, FILE *stream)
 {
   switch (c) {
     case '<':
-      printf("&lt;");
+      fprintf(stream, "&lt;");
       break;
     case '>':
-      printf("&gt;");
+      fprintf(stream, "&gt;");
       break;
     case '&':
-      printf("&ampq;");
+      fprintf(stream, "&ampq;");
       break;
     default:
-      putchar(c);
+      putc(c, stream);
       break;
   }
 }
 
 void
-compile_ts_node(TSNode node, const char *sources, unsigned int *last_end)
+compile_ts_node(TSNode node, const char *sources, unsigned int *last_end, FILE *stream)
 {
   TSNode child;
   unsigned int start, end, i;
@@ -38,28 +38,28 @@ compile_ts_node(TSNode node, const char *sources, unsigned int *last_end)
   if (ts_node_child_count(node)) {
     for (i = 0; i < ts_node_child_count(node); i++) {
       child = ts_node_child(node, i);
-      compile_ts_node(child, sources, last_end);
+      compile_ts_node(child, sources, last_end, stream);
     }
   } else {
     start = ts_node_start_byte(node);
     end = ts_node_end_byte(node);
 
     for (i = *last_end; i < start; i++) {
-      putchar_escaped(sources[i]);
+      putchar_escaped(sources[i], stream);
     }
 
-    printf("<span class=\"%s\">", ts_node_type(node));
+    fprintf(stream, "<span class=\"%s\">", ts_node_type(node));
     for (i = start; i < end; i++) {
-      putchar_escaped(sources[i]);
+      putchar_escaped(sources[i], stream);
     }
-    printf("</span>");
+    fprintf(stream, "</span>");
 
     *last_end = end;
   }
 }
 
 void
-compile_node(struct CMarkNode node)
+compile_node(struct CMarkNode node, FILE *stream)
 {
   size_t i;
   unsigned int last_end;
@@ -70,26 +70,26 @@ compile_node(struct CMarkNode node)
 
   switch (node.type) {
     case CMARK_ROOT:
-      printf("<main>");
-      COMPILE_CHILDREN(i, node);
-      printf("</main>");
+      fprintf(stream, "<main>");
+      COMPILE_CHILDREN(i, node, stream);
+      fprintf(stream, "</main>");
       break;
     case CMARK_NULL:
       break;
     case CMARK_HEADER:
-      printf("<h%d>", node.data.header.level);
-      COMPILE_CHILDREN(i, node);
-      printf("</h%d>", node.data.header.level);
+      fprintf(stream, "<h%d>", node.data.header.level);
+      COMPILE_CHILDREN(i, node, stream);
+      fprintf(stream, "</h%d>", node.data.header.level);
       break;
     case CMARK_PARAGRAPH:
-      printf("<p>");
-      COMPILE_CHILDREN(i, node);
-      printf("</p>");
+      fprintf(stream, "<p>");
+      COMPILE_CHILDREN(i, node, stream);
+      fprintf(stream, "</p>");
       break;
     case CMARK_ANCHOR:
-      printf("<a href=\"%s\">", node.data.anchor.href);
-      COMPILE_CHILDREN(i, node);
-      printf("</a>");
+      fprintf(stream, "<a href=\"%s\">", node.data.anchor.href);
+      COMPILE_CHILDREN(i, node, stream);
+      fprintf(stream, "</a>");
       break;
     case CMARK_CODE:
       if (node.data.code.is_block) {
@@ -107,26 +107,26 @@ compile_node(struct CMarkNode node)
           ts_tree = ts_parser_parse_string(ts_parser, NULL, node.data.code.content, strlen(node.data.code.content));
           ts_node = ts_tree_root_node(ts_tree);
 
-          printf("<pre><code>");
-          compile_ts_node(ts_node, node.data.code.content, &last_end);
-          printf("</pre></code>");
+          fprintf(stream, "<pre><code>");
+          compile_ts_node(ts_node, node.data.code.content, &last_end, stream);
+          fprintf(stream, "</pre></code>");
 
           ts_tree_delete(ts_tree);
         } else {
-          printf("<pre><code>%s</code></pre>", node.data.code.content);
+          fprintf(stream, "<pre><code>%s</code></pre>", node.data.code.content);
         }
       } else {
-        printf("<code>%s</code>", node.data.code.content);
+        fprintf(stream, "<code>%s</code>", node.data.code.content);
       }
       break;
     case CMARK_PLAIN:
-      printf("%s", node.data.plain);
+      fprintf(stream, "%s", node.data.plain);
       break;
     case CMARK_WHITESPACE:
-      printf(" ");
+      fprintf(stream, " ");
       break;
     case CMARK_BREAK:
-      printf("</br>");
+      fprintf(stream, "</br>");
       break;
   }
 }
@@ -136,12 +136,15 @@ main(void)
 {
   struct CMarkContext *ctx = cmark_create_context(stdin);
   struct CMarkNode root = cmark_parse(ctx);
+  FILE *file = stdout;
 
-  printf("<!DOCTYPE html><head><link rel=\"stylesheet\" href=\"/static/app.css\"/><meta charset=\"utf-8\"/></head><body>");
-  compile_node(root);
-  printf("</body>");
+  fprintf(file, "<!DOCTYPE html><head><link rel=\"stylesheet\" href=\"/static/app.css\"/><meta charset=\"utf-8\"/></head><body>");
+  compile_node(root, file);
+  fprintf(file, "</body>");
 
   cmark_free_node(root);
+
+  fclose(file);
 
   return 0;
 }
