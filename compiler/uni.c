02eec410323c2cbaf1660965150846f1d25878fa
Joseph DALY <joseph300905@gmail.com>
2024-09-12 14:29:22 +0200

    table of contents works now

--- compiler/uni.c
+++ compiler/uni.c
@@ -3,6 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define RES_BUF_SIZE 512
+#define HEADER_BUF_SIZE 128
+#define ANCHOR_BUF_SIZE 256
+#define CODE_BUF_SIZE 2048
+
 enum Flag {
   FLAG_PARAGRAPH = 1 << 1,
   FLAG_ANCHOR = 1 << 2,
@@ -52,23 +57,36 @@ main(void)
   struct Header *headers = malloc(sizeof(struct Header) * header_size);
   struct Header header;
   unsigned short flags = 0;
-  size_t buf_end = 0;
-  char buf[1024];
-  size_t inline_buf_end = 0;
-  char inline_buf[1024];
+  size_t res_buf_end = 0, header_buf_end = 0, anchor_buf_end = 0, code_buf_end = 0;
+  char res_buf[RES_BUF_SIZE], header_buf[HEADER_BUF_SIZE], anchor_buf[ANCHOR_BUF_SIZE], code_buf[CODE_BUF_SIZE];
   char code_lang[16];
   size_t i;
 
   char *ptr;
-  size_t *end;
+  size_t *end, size;
 
-  printf("<!DOCTYPE html><head><link rel=\"stylesheet\" href=\"/uni/static/app.css\"/><meta charset=\"utf-8\"/></head><body>");
+  printf("<!DOCTYPE html><html><head><link rel=\"stylesheet\" href=\"/uni/static/app.css\"/><meta charset=\"utf-8\"/></head><body><main>");
 
   while (1) {
     e = cmark_next(&p);
 
-    ptr = (flags & FLAG_ANCHOR) ? (inline_buf + inline_buf_end) : (buf + buf_end);
-    end = (flags & FLAG_ANCHOR) ? &inline_buf_end : &buf_end;
+    if (flags & FLAG_ANCHOR) {
+      ptr = anchor_buf + anchor_buf_end;
+      end = &anchor_buf_end;
+      size = ANCHOR_BUF_SIZE;
+    } else if (flags & (0b111 << 8)) {
+      ptr = header_buf + header_buf_end;
+      end = &header_buf_end;
+      size = HEADER_BUF_SIZE;
+    } else if (flags & (FLAG_MULTILINE_CODE)) {
+      ptr = code_buf + code_buf_end;
+      end = &code_buf_end;
+      size = CODE_BUF_SIZE;
+    } else {
+      ptr = res_buf + res_buf_end;
+      end = &res_buf_end;
+      size = RES_BUF_SIZE;
+    }
 
     switch (e.type) {
       case CMARK_HEADER:
@@ -77,7 +95,12 @@ main(void)
         header.text_end = 0;
         header.text[0] = '\0';
         header.id[0] = '\0';
-        printf("<h%d>", e.data.header_level);
+        continue;
+      case CMARK_BLOCKQUOTE_START:
+        printf("<blockquote>");
+        continue;
+      case CMARK_BLOCKQUOTE_END:
+        printf("</blockquote>");
         continue;
       case CMARK_PLAIN:
         if (!flags) {
@@ -90,7 +113,7 @@ main(void)
           header.text_end += e.data.plain.length;
         }
 
-        snprintf(ptr, 1023 - *end, "%.*s", (int) e.data.plain.length, e.data.plain.ptr);
+        snprintf(ptr, size - *end, "%.*s", (int) e.data.plain.length, e.data.plain.ptr);
         *end += e.data.plain.length;
         continue;
       case CMARK_ANCHOR_START:
@@ -98,9 +121,18 @@ main(void)
         continue;
       case CMARK_ANCHOR_END:
         flags &= ~FLAG_ANCHOR;
-        snprintf(buf + buf_end, 1023 - buf_end, "<a href=\"%.*s\">%.*s</a>", (int) e.data.anchor_end_href.length, e.data.anchor_end_href.ptr, (int) inline_buf_end, inline_buf);
-        buf_end += 15 + e.data.anchor_end_href.length + inline_buf_end;
-        inline_buf_end = 0;
+        if (flags & (0b111 << 8)) {
+          ptr = header_buf + header_buf_end;
+          end = &header_buf_end;
+          size = HEADER_BUF_SIZE;
+        } else {
+          ptr = res_buf + res_buf_end;
+          end = &res_buf_end;
+          size = RES_BUF_SIZE;
+        }
+        snprintf(ptr, size - *end, "<a href=\"%.*s\">%.*s</a>", (int) e.data.anchor_end_href.length, e.data.anchor_end_href.ptr, (int) anchor_buf_end, anchor_buf);
+        *end += 15 + e.data.anchor_end_href.length + anchor_buf_end;
+        anchor_buf_end = 0;
         continue;
       case CMARK_CODE_START:
         if (e.data.code.is_multi_line) {
@@ -111,39 +143,38 @@ main(void)
           code_lang[len] = '\0';
 
           printf("<pre><code>");
-          buf_end = 0;
         } else {
           flags |= FLAG_INLINE_CODE;
 
-          snprintf(ptr, 1023 - *end, "<code>");
+          snprintf(ptr, size - *end, "<code>");
           *end += 6;
         }
         continue;
       case CMARK_CODE_END:
         if (flags & FLAG_MULTILINE_CODE) {
           flags &= ~FLAG_MULTILINE_CODE;
-          buf[buf_end] = '\0';
-          char *highlighted = uni_highlight(buf, code_lang);
+          code_buf[code_buf_end] = '\0';
+          char *highlighted = uni_highlight(code_buf, code_lang);
           printf("%s", highlighted);
           uni_free_buffer(highlighted);
-          buf_end = 0;
+          code_buf_end = 0;
 
           printf("</code></pre>");
         } else {
           flags &= ~FLAG_INLINE_CODE;
-          snprintf(ptr, 1023 - *end, "</code>");
+          snprintf(ptr, size - *end, "</code>");
           *end += 7;
         }
         continue;
       case CMARK_BREAK:
         if (flags & FLAG_MULTILINE_CODE) {
-          snprintf(buf + buf_end, 1023 - buf_end, "\n");
-          buf_end += 1;
+          snprintf(code_buf + code_buf_end, size - code_buf_end, "\n");
+          code_buf_end += 1;
           continue;
         }
         
-        printf("%.*s", (int) buf_end, buf);
-        buf_end = 0;
+        printf("%.*s", (int) res_buf_end, res_buf);
+        res_buf_end = 0;
         if (flags & FLAG_PARAGRAPH) {
           printf("</p>");
         } else if (flags & (0b111 << 8)) {
@@ -156,7 +187,8 @@ main(void)
           headers[header_count] = header;
           header_count++;
 
-          printf("</h%d>", (flags >> 8) & 0b111);
+          printf("<h%d id=\"%s\">%.*s</h%d>", header.level, header.id, (int) header_buf_end, header_buf, header.level);
+          header_buf_end = 0;
         }
 
         flags = 0;
@@ -170,14 +202,13 @@ main(void)
     break;
   }
   
-  printf("<nav id=\"contents\"><ul>");
+  printf("</main><nav id=\"contents\"><ul>");
   for (i = 0; i < header_count; i++) {
     printf("<li><a href=\"#%s\">%s</a></li>", headers[i].id, headers[i].text);
   }
   free(headers);
 
-  printf("</ul></nav>");
-  printf("</body>");
+  printf("</ul></nav></body></html>");
 
   return 0;
 }
