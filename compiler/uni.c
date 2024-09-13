a26f7f2ce04ebf800612f1375db30c493c1168bb
Joseph DALY <joseph300905@gmail.com>
2024-09-13 12:07:57 +0200

    compile lists

--- compiler/uni.c
+++ compiler/uni.c
@@ -57,6 +57,8 @@ main(void)
   struct Header *headers = malloc(sizeof(struct Header) * header_size);
   struct Header header;
   unsigned short flags = 0;
+  char is_newline = 1;
+  char is_list = 0;
   size_t res_buf_end = 0, header_buf_end = 0, anchor_buf_end = 0, code_buf_end = 0;
   char res_buf[RES_BUF_SIZE], header_buf[HEADER_BUF_SIZE], anchor_buf[ANCHOR_BUF_SIZE], code_buf[CODE_BUF_SIZE];
   char code_lang[16];
@@ -69,6 +71,13 @@ main(void)
 
   while (1) {
     e = cmark_next(&p);
+
+    if (is_newline && is_list && CMARK_LIST_ITEM != e.type) {
+      printf("</ul>");
+      is_list = 0;
+    }
+
+    is_newline = e.type == CMARK_BREAK;
 
     if (flags & FLAG_ANCHOR) {
       ptr = anchor_buf + anchor_buf_end;
@@ -95,6 +104,13 @@ main(void)
         header.text_end = 0;
         header.text[0] = '\0';
         header.id[0] = '\0';
+        continue;
+      case CMARK_LIST_ITEM:
+        if (!is_list) {
+          printf("<ul>");
+        }
+        printf("<li>");
+        is_list = 1;
         continue;
       case CMARK_BLOCKQUOTE_START:
         printf("<blockquote>");
@@ -189,6 +205,10 @@ main(void)
 
           printf("<h%d id=\"%s\">%.*s</h%d>", header.level, header.id, (int) header_buf_end, header_buf, header.level);
           header_buf_end = 0;
+        }
+
+        if (is_list) {
+          printf("</li>");
         }
 
         flags = 0;
